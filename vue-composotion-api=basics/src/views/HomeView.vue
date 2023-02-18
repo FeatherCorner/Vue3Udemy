fd3f81b631e3cdbbf1a01ece06a958726f6bed1a
Erik <Erik@VITTOOLS.LOCAL>
2023-02-18 13:12:26 +0100

    Start

---
+++ vue-composotion-api=basics/src/views/HomeView.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="home">
+    
+        <h2>{{ appTitle }}</h2>
+    
+    
+    
+        <h3>{{ counterData.title }}:</h3>
+    
+    
+    
+        <div>
+    
+            <button @click="decreaseCounter" class="btn">-</button>
+    
+            <span class="counter">{{ counterData.count }}</span>
+    
+            <button @click="increaseCounter(counterData.amount)" class="btn">+</button>
+    
+        </div>
+    
+    
+    
+        <div class="edit">
+    
+            <h4>Edit counter title:</h4>
+    
+            <input v-model="counterData.title" type="text">
+    
+        </div>
+    
+    </div>
+</template>
+
+<script setup >
+import { reactive } from 'vue'
+const appTitle = 'My Ok Counter App'
+// const counter = ref(0),
+//       counterTitle = ref('My Counter')
+const counterData = reactive({
+  count: 0,
+  title: 'My Counter',
+  amount: 3
+})
+const increaseCounter = (amount) => {
+
+  counterData.count+=amount
+}
+const decreaseCounter = () => {
+  counterData.count--
+}
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+.edit {
+  margin-top: 60px;
+}
+</style>
