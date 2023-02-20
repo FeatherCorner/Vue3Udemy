b2b0e965d851366614c8582c4c51cbe7712983da
Erik <Erik@VITTOOLS.LOCAL>
2023-02-20 11:37:23 +0100

    module 25

--- vue-composotion-api=basics/src/views/HomeView.vue
+++ vue-composotion-api=basics/src/views/HomeView.vue
@@ -11,14 +11,13 @@
     
         <div>
     
-            <button @click="decreaseCounter" class="btn">-</button>
-    
+            
+            <button @click="decreaseCounter(counterData.amount, $event)" class="btn">-</button>
             <span class="counter">{{ counterData.count }}</span>
-    
             <button @click="increaseCounter(counterData.amount)" class="btn">+</button>
     
         </div>
-    
+        <p>This counter is {{ oddOrEven }}</p>
     
     
         <div class="edit">
@@ -28,27 +27,41 @@
             <input v-model="counterData.title" type="text">
             <input v-model="counterData.amount" type="number">
     
+
         </div>
-    
+        
     </div>
 </template>
 
 <script setup >
-import { reactive } from 'vue'
+import { reactive, computed, watch } from 'vue'
 const appTitle = 'My Ok Counter App'
 // const counter = ref(0),
 //       counterTitle = ref('My Counter')
+const oddOrEven = computed(() => {
+   if(counterData.count %2 === 0){
+    return 'even'
+   } else {
+    return 'odd'
+   }
+})
 const counterData = reactive({
   count: 0,
   title: 'My Counter',
   amount: 3
 })
+watch(()=> counterData.count, (newCount, oldCount)=>{
+    if(newCount=== 20){
+        alert('Way to go! You made it to 20!!')
+    }
+})
 const increaseCounter = (amount) => {
 
   counterData.count+=amount
 }
-const decreaseCounter = () => {
-  counterData.count--
+const decreaseCounter = (amount, e) => {
+  console.log(e)
+    counterData.count-=amount
 }
 </script>
 
